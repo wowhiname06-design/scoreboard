8ed276be72e6fde2cb8bb5ca50063aa2f99c2a3d
wowhiname06-design <wowhiname06@gmail.com>
2026-09-03 19:29:13 +0900

    Nightbot 화면 제거

--- app.py
+++ app.py
@@ -245,9 +245,4 @@ def live_tracker():
     else:
         status_area.info("🔵 대기 중입니다. 상단 설정 메뉴에서 **[▶ 추적 시작]** 버튼을 눌러주세요.")
 
-live_tracker()
-
-
-from nightbot_panel import render_nightbot_panel
-
-render_nightbot_panel()
+live_tracker()
